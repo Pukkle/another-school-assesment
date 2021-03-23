69038488d7f048b6e8b2cef2f41b07feb3a89ef7
pukkle_ <timmsr@stu.otc.school.nz>
2021-03-23 15:04:54 +1300

    qweruiopasdfghjklzxcvbnm

--- another school assesment.py
+++ another school assesment.py
@@ -1,3 +1,23 @@
 n = int(input("what is your number?"))
 n = (n + 1) * (n / 2)
 print("you chose {}".format(n))
+
+print("")
+
+hi = 1
+goodbye = 1
+awkward_silence = 0
+while awkward_silence < 12:
+    awkward_silence = 1 + awkward_silence
+    hi = 1
+    while hi < 13:
+        print("{} * {} = {}".format(hi, awkward_silence, goodbye))
+        hi += 1
+        goodbye = hi * awkward_silence
+
+print("")
+
+for i in range(9999):
+    if i == 456:
+      print(i)
+
