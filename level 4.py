7c30c022d16d74f235841988be2ef5a644342e66
pukkle_ <timmsr@stu.otc.school.nz>
2021-04-13 14:53:06 +1200

    qweruiopasdfghjklzxcvbnm

---
+++ level 4.py
@@ -0,0 +1,11 @@
+a = 18
+b = 7
+c = 5
+d = a * b
+e = d // 5
+f = e // 7
+g = e % 7
+h = d % 5
+print("Whole chocolate bars each: {}" .format(f))
+print("Extra squares each: {}" .format(g))
+print("Squares left over: {}" .format(h))
