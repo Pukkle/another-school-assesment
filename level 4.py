30d416adf5b5a8248c0ad4cfefd67fd4b1afca4c
pukkle_ <timmsr@stu.otc.school.nz>
2021-04-13 14:54:13 +1200

    hello

--- level 4.py
+++ level 4.py
@@ -6,6 +6,6 @@ e = d // 5
 f = e // 7
 g = e % 7
 h = d % 5
-print("Whole chocolate bars each: {}" .format(f))
-print("Extra squares each: {}" .format(g))
-print("Squares left over: {}" .format(h))
+print("Whole chocolate bars each: {}".format(f))
+print("Extra squares each: {}".format(g))
+print("Squares left over: {}".format(h))
