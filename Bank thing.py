b328374410bd6ff79d3d63f8439582561892682d
pukkle_ <timmsr@stu.otc.school.nz>
2021-05-27 13:30:02 -1200

    Bank thing

--- Bank thing.py
+++ Bank thing.py
@@ -14,16 +14,23 @@ user_13 = 0
 user_14 = 0
 user_15 = 0
 user_16 = 0
-name_list = ["User_1", "User_2", "User_3", "User_4", "User_5", "User_6", "User_7", "User_8", "User_9", "User_10", "User_11", "User_12", "User_13", "User_14", "User_15", "User_16"]
-username = input("What is your name? ")
-if username in name_list:
+name_list = ["Account_1", "Account_2", "Account_3", "Account_4", "Account_5", "Account_6", "Account_7", "Account_8", "Account_9", "Account_10", "Account_11", "Account_12", "Account_13", "Account_14", "Account_15", "Account_16"]
+account = input("Which account would you like to access? ")
+ if account == "Account_1":
+     code_1 = int(input("Please input the access code. "))
+     if code_1 == "1234":
+         choice_1 = input("would you like to withdraw, deposit, or borrow money? ")
+         if choice_1 == "withdraw":
+             withdraw_1 = int(input("How much would you like to withdraw? "))
+             user_1 -= withdraw_1
+             print("{} has been deducted from your account".format(withdraw_1))
+         elif choice_1 == "deposit":
+             deposit_1 = int(input("How much would you like to deposit? "))
+             user_1 += deposit_1
+             print("{} has been added to your account".format(deposit_1))
+         else:
+             print("Please enter a valid option.")
+     else:
+         print("That code is incorrect.")
 
-else:
-  replaced_name = input("Where would you like to add your name? ")
-  if replaced_name in name_list:
-        position = name_list.index(replaced_name)
-        name_list[position] = username
-        print(name_list)
-        if replaced_name == "User_1":
-            user_1 = username
 
