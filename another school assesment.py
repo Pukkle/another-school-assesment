5624d02f76d4e50084e50a6d7af96a5ecfdd3578
timmsr <timmsr@stu.otc.school.nz>
2021-09-17 12:32:02 +1200

    million dollar question

--- another school assesment.py
+++ another school assesment.py
@@ -1,55 +1,18 @@
-n = int(input("what is your number?"))
-n = (n + 1) * (n / 2)
-print("you chose {}".format(n))
-
-print("")
-
-hi = 1
-goodbye = 1
-awkward_silence = 0
-while awkward_silence < 12:
-    awkward_silence = 1 + awkward_silence
-    hi = 1
-    while hi < 13:
-        print("{} * {} = {}".format(hi, awkward_silence, goodbye))
-        hi += 1
-        goodbye = hi * awkward_silence
-
-print("")
-
-for i in range(9999):
-    if i == 456:
-          a = str(i)
-          b = a.zfill(4)
-          print(b)
-
-print("")
-x = 1
-y = 2
-z = 1
-n = int(input("What is your number"))
-while z <= n:
-    z += 1
-    y = 2
-    while y <= n:
-        if z * y == n:
-            x = 2
-        y = y + 1
-if n == 1:
-    x = 2
-if x == 2:
-    print("It is not a prime number")
-if x == 1:
-    print("It is a prime number")
-
-
-
-
-
-
-
-
-
-
-
-
+loop = 0
+start = int(input("What number would you like to start with?"))
+while loop < 1:
+    if (start - 1) % 3 == 0:
+        if start % 2 == 0:
+            if (start - 1) % 9 == 0:
+                print("Ending: {}".format((start - 1)/3))
+                start *= 2
+            else:
+                start -= 1
+                start /= 3
+                print(start)
+        else:
+            start *= 2
+    else:
+        start *= 2
+else:
+    print("Done.")
